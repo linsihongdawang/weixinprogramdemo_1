e8e7e326eefebebe9a4a9563cbae8655a3866746
linsihongdawang <460580911@qq.com>
2020-03-14 21:15:24 +0800

    组件化开发

---
+++ components/my-style/my-style.js
@@ -0,0 +1,31 @@
+// components/my-style/my-style.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  // 该属性类似于vue里面的propers属性,目的是用来向组件传递数据
+  properties: {
+   title:{
+     type:String,
+     value:"哈哈哈"
+   },
+   content:{
+     type:String,
+     value:"嘿嘿嘿"
+   }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ pages/wxss/wxss.js
@@ -0,0 +1,77 @@
+// pages/wxss/wxss.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nowTime:new Date().toLocaleString(),
+    movies:['hjw','dlam','dmkj'],
+    counter:1,
+  },
+  handleIncrement(){
+    this.setData({
+      counter:this.data.counter+1
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //设置定时器方法使得计数时间为1000毫秒
+    setInterval(()=>{
+       this.setData({
+         nowTime: new Date().toLocaleString()
+       })
+    },1000)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
