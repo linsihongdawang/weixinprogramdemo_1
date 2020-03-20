309eb423978fd714281d8e7468d5db3152d876e3
linsihongdawang <460580911@qq.com>
2020-03-20 18:05:23 +0800

    视频播放demo

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,71 @@
 // pages/home/home.js
+//弹幕生成随机颜色的函数
+function getRandomColor() {
+  let rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length == 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+   videoList:[
+     {id:'1',
+     title:'第一个视频',
+       videoUrl:'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'},
+     {
+       id: '2',
+       title: '第二个视频',
+       videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+     },
+     {
+       id: '3',
+       title: '第三个视频',
+       videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+     }  
+   ],
+   src:'',
+   danmuTxt:''
+  },
+  //这里是获取弹幕的事件
+  getDanmu(txtValue){
+    console.log(txtValue)
+    this.setData({
+      danmuTxt:txtValue.detail.value
+    })
+  },
+  //这里是发送弹幕的事件
+  sendDanmu(){
+    let text=this.data.danmuTxt
+    this.videoCtx.sendDanmu({
+      text:text,
+      color:getRandomColor()
+    })
 
   },
-
+  //这里是播放视频的事件
+  playVideo(event){
+  // console.log(event)  
+  //停止之前正在播放的视频
+   this.videoCtx.stop();
+   //更新新的视频的播放地址
+   this.setData({
+     src:event.currentTarget.dataset.url
+   })
+   //播放新的视频
+   this.videoCtx.play()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.videoCtx=wx.createVideoContext('video_1')
   },
 
   /**
